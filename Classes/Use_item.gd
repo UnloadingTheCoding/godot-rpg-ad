3e8e5417ed7f155f3acd510973f43f5b2f0885d2
Fred <Fred@Legendary.com>
2024-03-20 16:14:37 -0700

    added master list for in game inventory and created use_item_type resource to populate list for later calling

--- Classes/Use_item.gd
+++ Classes/Use_item.gd
@@ -4,9 +4,9 @@ extends Node
 class_name Use_item
 
 
-var icon: Sprite2D
-var item_name: String
-var description: String
+@export var icon: Texture
+@export var item_name: String
+@export var description: String
 
 
 # Add effects and use functions
--- Scenes/ShopMenu/shop_menu.gd
+++ Scenes/ShopMenu/shop_menu.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
 @onready var shop_menu_container = $ShopMenuContainer
+@onready var buy_inventory = $ShopMenuContainer/PanelContainer/MarginContainer/VBoxContainer/CenterPanels/Selection/BuyInventory
+@onready var sell_inventory = $ShopMenuContainer/PanelContainer/MarginContainer/VBoxContainer/CenterPanels/Selection/SellInventory
 
 
 var selling: bool = false
@@ -35,3 +37,16 @@ func close_menu():
 	if selling == true and Input.is_action_pressed("back"):
 		shop_menu_container.visible = false
 		get_tree().paused = false
+
+
+func _on_buy_pressed():
+	buy_inventory.visible = true
+	sell_inventory.visible = false
+
+func _on_sell_pressed():
+	buy_inventory.visible = false
+	sell_inventory.visible = true
+
+func _on_exit_pressed():
+	shop_menu_container.visible = false
+	get_tree().paused = false

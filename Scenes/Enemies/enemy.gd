9c4a25ed00a2218c73b574d2b7e780ac81118b06
Fred <Fred@Legendary.com>
2024-04-22 16:53:29 -0700

    Added Enemy Scene

---
+++ Scenes/Enemies/enemy.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+class_name Enemy
+
+@export var health : int
+@export var mana : int
+@export var attack : int
+@export var defense : int
+@export var magic_attack : int
+@export var magic_defense : int
+
+
